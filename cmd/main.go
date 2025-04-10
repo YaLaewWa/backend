be3c64a04b93b45ccb7cacf140ae8f4e50769463
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-10 22:21:06 +0700

    feat: add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"socket/internal/config"
+	"socket/internal/server"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("hello")
+	config := config.Load()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	s := server.NewServer(config.Server)
+	s.Start(ctx, stop)
 }
