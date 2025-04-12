a7fb6c0f1f7ce13e040ecec027154e53b364ffea
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-13 00:16:00 +0700

    feat: add the handler to server

--- internal/server/route.go
+++ internal/server/route.go
@@ -1,11 +1,7 @@
 package server
 
 import (
-	"log"
-	"socket/internal/hub"
-
 	"github.com/gofiber/contrib/websocket"
-	"github.com/google/uuid"
 )
 
 func (s *Server) initRoutes() {
@@ -14,44 +10,7 @@ func (s *Server) initRoutes() {
 }
 
 func (s *Server) initSocket() {
-	s.app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		id := uuid.New() //TODO: change to user id in the future
-		hubChannel := make(chan []byte)
-		payload := &hub.RegisterPayload{
-			Channel: hubChannel,
-			ID:      id,
-		}
-		s.messageHub.Register <- payload //register new connection to hub
-
-		defer func() { //Porperly close connection
-			s.messageHub.Unregister <- id
-			c.Close()
-		}()
-
-		for index := range s.db.Message { // send old message to the new user
-			err := c.WriteMessage(1, s.db.Message[index])
-			if err != nil {
-				log.Println("read:", err)
-				break
-			}
-		}
-
-		for {
-			_, msg, err := c.ReadMessage()
-			if err != nil {
-				log.Println("read:", err)
-				break
-			}
-			s.db.Message = append(s.db.Message, msg)
-			s.messageHub.Broadcast <- msg
-
-			msg = <-hubChannel
-			err = c.WriteMessage(1, msg)
-			if err != nil {
-				break
-			}
-		}
-	}))
+	s.app.Get("/ws", websocket.New(s.handler.socketMessageHandler.InitConnection))
 
 }
 
