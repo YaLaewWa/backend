61e80815e43a3e5594920d6864fe31921de24f0e
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-13 14:51:04 +0700

    feat: define message ports

--- internal/core/ports/message_socket_port.go
+++ internal/core/ports/message_socket_port.go
@@ -1,6 +1,20 @@
 package ports
 
-import "github.com/gofiber/contrib/websocket"
+import (
+	"socket/internal/core/domain"
+
+	"github.com/gofiber/contrib/websocket"
+)
+
+type MessageRepository interface {
+	Create(msg *domain.Message) error
+	GetAll() ([]domain.Message, error)
+}
+
+type MessageService interface {
+	Create(msg *domain.Message) error
+	GetAll() ([]domain.Message, error)
+}
 
 type MessageSocketHandler interface {
 	InitConnection(c *websocket.Conn)
