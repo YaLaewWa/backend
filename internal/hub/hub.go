b711a2f5c00a684b6ece130584b32df148e6731c
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-14 14:03:11 +0700

    feat: Implement function to broadcast current online users

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -1,5 +1,10 @@
 package hub
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type RegisterPayload struct {
 	Channel  chan []byte
 	Username string
@@ -34,4 +39,30 @@ func (h *Hub) Run() {
 			}
 		}
 	}
+}
+
+func (h *Hub) broadcastOnlineUsers() {
+	onlineUsers := make([]string, len(h.Clients))
+	for username := range h.Clients {
+		onlineUsers = append(onlineUsers, username)
+	}
+
+	// not too sure about this yet but
+	// our json may have to look like {"type":"?", "content":?}
+	// because we can have many kind of stuff to send over socket
+	// not too sure what to call each field yet
+	msg := map[string]interface{}{
+		"type":         "online_users",
+		"online_users": onlineUsers,
+	}
+
+	data, err := json.Marshal(msg)
+	if err != nil {
+		log.Println("error: ", err)
+	} else {
+		for id := range h.Clients {
+			h.Clients[id] <- data
+		}
+	}
+
 }
