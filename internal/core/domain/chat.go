2cade37e3b041ab21fdfd0d1e5c5bf99ef39e1e0
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-21 09:58:05 +0700

    chore: remove unneeded comment

--- internal/core/domain/chat.go
+++ internal/core/domain/chat.go
@@ -8,7 +8,7 @@ import (
 
 type Chat struct {
 	ID      uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Name    string    // Is this field needed? I just thought of this and it isn't in the design.
+	Name    string
 	IsGroup bool
 	Members []User `gorm:"many2many:chat_members;"`
 }
