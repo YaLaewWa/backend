dc256f6a8b21908ac7730793211c07f3d07a7edf
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-22 20:45:10 +0700

    feat: add template for service

--- internal/core/services/message_queue_service.go
+++ internal/core/services/message_queue_service.go
@@ -1,1 +1,32 @@
 package services
+
+import (
+	"socket/internal/core/domain"
+	"socket/internal/core/ports"
+
+	"github.com/google/uuid"
+)
+
+type MessageQueueService struct {
+	repo ports.MessageQueueRepository
+}
+
+func NewMessageQueueService(repo ports.MessageQueueRepository) ports.MessageQueueService {
+	return &MessageQueueService{repo: repo}
+}
+
+func (c *MessageQueueService) Create(username string, chatID uuid.UUID) error {
+	return nil
+}
+
+func (c *MessageQueueService) Get(username string) ([]domain.MessageQueue, error) {
+	return nil, nil
+}
+
+func (c *MessageQueueService) ReceiveMessage(username string, chatID uuid.UUID) error {
+	return nil
+}
+
+func (c *MessageQueueService) ReadMessage(username string, chatID uuid.UUID) error {
+	return nil
+}
