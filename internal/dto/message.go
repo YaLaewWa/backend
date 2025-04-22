e6d00336c4bde0f91dc3481ead7657d5e7af1bf8
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-22 08:34:32 +0700

    feat: define message sent request dto

--- internal/dto/message.go
+++ internal/dto/message.go
@@ -1,9 +1,19 @@
 package dto
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type MessageResponse struct {
 	CreateAt time.Time `json:"create_at"`
 	Username string    `json:"username"`
 	Content  string    `json:"message"`
+}
+
+type MessageRequest struct {
+	Type    string    `json:"type"`
+	ChatID  uuid.UUID `json:"chat_id"`
+	Content string    `json:"content"`
 }
