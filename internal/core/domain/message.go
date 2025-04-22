3daa3c4916a9a9f4ea50e3e0f09f472e34150a44
Chantouch Narasetaphan <inecrft4747@gmail.com>
2025-04-22 17:44:10 +0700

    refactor: remove updateAt field

--- internal/core/domain/message.go
+++ internal/core/domain/message.go
@@ -10,7 +10,6 @@ import (
 type Message struct {
 	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	CreateAt time.Time `gorm:"autoCreateTime"`
-	UpdateAt time.Time `gorm:"autoUpdateTime"`
 	Username string
 	Content  string
 	ChatID   uuid.UUID
--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -10,7 +10,6 @@ import (
 type User struct {
 	ID       uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	CreateAt time.Time `gorm:"autoCreateTime"`
-	UpdateAt time.Time `gorm:"autoUpdateTime"`
 	Username string    `gorm:"unique" validate:"required"`
 	Password string    `validate:"required,min=8"`
 }
