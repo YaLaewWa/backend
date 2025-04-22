17d43a8e6a6a169506b50551b47eacdbd3bc9057
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-22 20:52:26 +0700

    feat: add logic to service

--- internal/core/services/message_queue_service.go
+++ internal/core/services/message_queue_service.go
@@ -16,17 +16,35 @@ func NewMessageQueueService(repo ports.MessageQueueRepository) ports.MessageQueu
 }
 
 func (c *MessageQueueService) Create(username string, chatID uuid.UUID) error {
-	return nil
+	return c.repo.Create(&domain.MessageQueue{Username: username, ChatID: chatID, Count: 0})
 }
 
 func (c *MessageQueueService) Get(username string) ([]domain.MessageQueue, error) {
-	return nil, nil
+	return c.repo.GetAll(username)
 }
 
 func (c *MessageQueueService) ReceiveMessage(username string, chatID uuid.UUID) error {
+	queue, err := c.repo.GetQueue(username, chatID)
+	if err != nil {
+		return err
+	}
+	queue.Count += 1
+	err = c.repo.Update(queue)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (c *MessageQueueService) ReadMessage(username string, chatID uuid.UUID) error {
+	queue, err := c.repo.GetQueue(username, chatID)
+	if err != nil {
+		return err
+	}
+	queue.Count = 0
+	err = c.repo.Update(queue)
+	if err != nil {
+		return err
+	}
 	return nil
 }
